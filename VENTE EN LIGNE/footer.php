dcb18a3df0023edb065ced927a35717bed758a7b
MeriteDs <164486112+MeriteDs@users.noreply.github.com>
2024-07-04 17:41:37 +0200

    Delete VENTE EN LIGNE directory

--- VENTE EN LIGNE/footer.php
+++
@@ -1,9 +0,0 @@
-
-<footer class="col-sm-12" style="text-align: center; margin-top: 100px;">
-    <script>
-        <script src="bootstrap/js/jquery-3.3.1.js"></script>
-        <script src="bootstrap/js/bootstrap.js"></script>
-    </script>
-
-</footer>
-</html>
